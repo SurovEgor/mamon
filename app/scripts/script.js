15f49937d30bbcdf27fae6a08c7b495b78aa54ad
Egor Surov <surovegor.se@gmail.com>
2020-06-15 12:27:02 +0400

    Адаптивное меню

---
+++ app/scripts/script.js
@@ -0,0 +1,8 @@
+const buttonMenu = document.querySelector(".menu-toggle"),
+      menu = document.querySelector(".header__right");
+
+buttonMenu.addEventListener("click", () => {
+    buttonMenu.classList.toggle("is-active");
+    menu.classList.toggle("open");
+
+});
